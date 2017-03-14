d31a3f0531b6f7a661fe38f86dc6feb03e33fde5
Robert Hardy <robbert@gmail.com>
2017-03-14 19:38:10 +0000

    Nullify the update.
    Builds.

--- app/frontend/PanelWordList/State.elm
+++ app/frontend/PanelWordList/State.elm
@@ -10,19 +10,4 @@ init =
 
 
 update msg model =
-    case model.word of
-        Nothing ->
-            ( model, Cmd.none )
-
-        Just word ->
-            case msg of
-                GetReply ->
-                    ( model, getReply word )
-
-                Reply reply ->
-                    ( { model | reply = Just reply }
-                    , Cmd.none
-                    )
-
-                otherwise ->
-                    ( model, Cmd.none )
+    ( model, Cmd.none )
