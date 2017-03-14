3b2e14db6e1bed32edfc7b58b7c1ef31f147f789
Robert Hardy <robbert@gmail.com>
2017-03-14 20:04:22 +0000

    Fix.

--- app/frontend/AppMain/View.elm
+++ app/frontend/AppMain/View.elm
@@ -7,46 +7,41 @@ import AppMain.Types exposing (..)
 
 
 root model =
-    case model.word of
-        Nothing ->
-            text ""
+    let
+        panelHeader =
+            div
+                [ class "panel-heading" ]
+                [ div
+                    [ class "panel-title" ]
+                    [ h1 [] [ text "Word echo" ] ]
+                ]
 
-        Just w ->
-            let
-                panelHeader =
-                    div
-                        [ class "panel-heading" ]
-                        [ div
-                            [ class "panel-title" ]
-                            [ h1 [] [ text "Word echo" ] ]
-                        ]
+        panelBody =
+            div
+                [ class "panel-body" ]
+                [ text "Click a word" ]
 
-                panelBody =
-                    div
-                        [ class "panel-body" ]
-                        [ text "Click a word" ]
-
-                panelList =
-                    ul [ class "list-group" ]
-                        [ li
-                            [ class "list-group-item"
-                            , onClick (WordSelected "word 1")
-                            , style [("cursor", "pointer")]
-                            ]
-                            [ text "word 1" ]
-                        , li
-                            [ class "list-group-item"
-                            , onClick (WordSelected "word 2")
-                            , style [("cursor", "pointer")]
-                            ]
-                            [ text "word 2" ]
-                        , li
-                            [ class "list-group-item"
-                            , onClick (WordSelected "word 3")
-                            , style [("cursor", "pointer")]
-                            ]
-                            [ text "word 3" ]
-                        ]
+        panelList =
+            ul [ class "list-group" ]
+                [ li
+                    [ class "list-group-item"
+                    , onClick (WordSelected "word 1")
+                    , style [("cursor", "pointer")]
+                    ]
+                    [ text "word 1" ]
+                , li
+                    [ class "list-group-item"
+                    , onClick (WordSelected "word 2")
+                    , style [("cursor", "pointer")]
+                    ]
+                    [ text "word 2" ]
+                , li
+                    [ class "list-group-item"
+                    , onClick (WordSelected "word 3")
+                    , style [("cursor", "pointer")]
+                    ]
+                    [ text "word 3" ]
+                ]
             in
                 div [ class "container" ]
                     [ div [ class "panel panel-default" ]
